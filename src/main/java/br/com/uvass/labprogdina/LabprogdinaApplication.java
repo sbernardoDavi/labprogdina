5f9b708450b8fba5e10110e875673d7506499b31
Davi Bernardo <49735486+sbernardoDavi@users.noreply.github.com>
2021-09-25 15:18:11 -0300

    Feat: Configuração do ambiente

---
+++ src/main/java/br/com/uvass/labprogdina/LabprogdinaApplication.java
@@ -0,0 +1,14 @@
+package br.com.uvass.labprogdina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabprogdinaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LabprogdinaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/uvass/labprogdina/LabprogdinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uvass.labprogdina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabprogdinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
